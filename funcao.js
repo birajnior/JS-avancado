5d327dd7680a1bbba9424acaeeba354ee188b96a
birajnior <ubiratanbernardo@gmail.com>
2023-03-16 21:30:02 -0300

    primeiras aulas

---
+++ funcao.js
@@ -0,0 +1,8 @@
+function sayHello(name, lastName) {
+    console.log(`Olá ${name} ${lastName}. Bem-vindo(a) à Dev Samurai!`);
+}
+
+sayHello('Weverton', 'Fagner')
+sayHello('Ubiratan', 'Júnior')
+sayHello('Mika', 'Gomes')
+
---
+++ objetoArguments.js
@@ -0,0 +1,14 @@
+function sum(n1, n2) {
+    // console.log(typeof arguments);
+    // console.log(arguments);
+    
+    let res = 0
+
+    for (const n of arguments) {
+        res += n
+    }
+
+    return res
+}
+
+console.log(sum(1, 1, 3, 10, 4, 1, 3, 4));

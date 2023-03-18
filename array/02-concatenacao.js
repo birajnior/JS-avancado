8edfa5f1608755bfc580cacd19eb30ad58b7e457
birajnior <ubiratanbernardo@gmail.com>
2023-03-18 16:58:45 -0300

    Finalizando aulas de Array.

--- array/02-concatenacao.js
+++ array/02-concatenacao.js
@@ -1,4 +1,8 @@
 let set1 = [1, 2, 3, 4, 5]
 let set2 = [6, 7, 8, 9, 10]
+let set3 = [11, 12, 13]
 
-console.log (set1 + set2);
+console.log(set1 + set2);
+console.log(set1, set2);
+
+console.log(set1.concat(set2).concat(set3));
